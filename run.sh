6b54c522c034e6dad205e3f66edc4b9d0b133ee5
Simone Basso <bassosimone@gmail.com>
2025-01-11 14:57:04 +0100

    feat: simple kernel that boots
    See https://operating-system-in-1000-lines.vercel.app/en/04-boot

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -euxo pipefail
+
+# QEMU file path
+QEMU=qemu-system-riscv32
+
+CC=/opt/homebrew/opt/llvm/bin/clang
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
+
+# Build the kernel
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c
+
+# Start QEMU
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot -kernel kernel.elf

718c09021ccbfe32d1d3ff5507a8de895ba751a8
Simone Basso <bassosimone@gmail.com>
2025-01-11 23:07:59 +0100

    feat: start sketching out C library
    See https://operating-system-in-1000-lines.vercel.app/en/06-libc

--- common.c
+++ common.c
@@ -1,5 +1,28 @@
 #include "common.h"
 
+void *memcpy(void *dst, const void *src, size_t n) {
+    uint8_t *d = (uint8_t *) dst;
+    const uint8_t *s = (const uint8_t *) src;
+    while (n--)
+        *d++ = *s++;
+    return dst;
+}
+
+void *memset(void *buf, char c, size_t n) {
+    uint8_t *p = (uint8_t *) buf;
+    while (n--)
+        *p++ = c;
+    return buf;
+}
+
+char *strcpy(char *dst, const char *src) {
+    char *d = dst;
+    while (*src)
+        *d++ = *src++;
+    *d = '\0';
+    return dst;
+}
+
 void putchar(char ch);
 
 void printk(const char *fmt, ...) {

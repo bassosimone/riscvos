5688949ef1023138f0ccfc8869b95570cca456e5
Simone Basso <bassosimone@gmail.com>
2025-01-12 13:52:33 +0100

    feat: implement user mode
    See https://operating-system-in-1000-lines.vercel.app/en/13-user-mode

--- kernel.h
+++ kernel.h
@@ -82,4 +82,10 @@ struct process {
 #define PAGE_X    (1 << 3)   // Executable
 #define PAGE_U    (1 << 4)   // User (accessible in user mode)
 
+// The base virtual address of an application image. This needs to match the
+// starting address defined in `user.ld`.
+#define USER_BASE 0x1000000
+
+#define SSTATUS_SPIE (1 << 5)
+
 #endif // __KERNEL_H__

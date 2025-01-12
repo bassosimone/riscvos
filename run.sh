d6d9798ac9e0c009ab37432f9ab6d94a1794c149
Simone Basso <bassosimone@gmail.com>
2025-01-12 22:29:43 +0100

    refactor: move linker scripts

--- run.sh
+++ run.sh
@@ -10,12 +10,12 @@ CFLAGS="-std=c17 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib
 OBJCOPY=/opt/homebrew/opt/llvm/bin/llvm-objcopy
 
 # Build the shell (application)
-$CC $CFLAGS -Wl,-Tuser.ld -Wl,-Map=shell.map -o shell.elf bin/shell/shell.c lib/libkern/stdio.c lib/libkern/string.c lib/libc/riscv32/syscall.c lib/libc/riscv32/crt0.c lib/libc/stdio.c lib/libc/stdlib.c
+$CC $CFLAGS -Wl,-Tlib/libc/riscv32/user.ld -Wl,-Map=shell.map -o shell.elf bin/shell/shell.c lib/libkern/stdio.c lib/libkern/string.c lib/libc/riscv32/syscall.c lib/libc/riscv32/crt0.c lib/libc/stdio.c lib/libc/stdlib.c
 $OBJCOPY --set-section-flags .bss=alloc,contents -O binary shell.elf shell.bin
 $OBJCOPY -Ibinary -Oelf32-littleriscv shell.bin shell.bin.o
 
 # Build the kernel
-$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf lib/libkern/stdio.c lib/libkern/string.c kernel/riscv32/console.c kernel/riscv32/main.c kernel/riscv32/mm.c kernel/riscv32/proc.c kernel/riscv32/sbi.c kernel/riscv32/trap.c shell.bin.o
+$CC $CFLAGS -Wl,-Tkernel/riscv32/kernel.ld -Wl,-Map=kernel.map -o kernel.elf lib/libkern/stdio.c lib/libkern/string.c kernel/riscv32/console.c kernel/riscv32/main.c kernel/riscv32/mm.c kernel/riscv32/proc.c kernel/riscv32/sbi.c kernel/riscv32/trap.c shell.bin.o
 
 # Start QEMU
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot -kernel kernel.elf
